2ba0ff61bd201ba45781107064049c076d13ff12
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2024-01-24 22:36:00 +0100

    feat: endpoint to get my licenses

--- src/api/licenses.api.ts
+++ src/api/licenses.api.ts
@@ -1,1 +1,14 @@
 import { basePath } from "./utils/config";
+
+export const getMyLicensesAPI = async (token: string): Promise<any> => {
+  const params = {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${token}`,
+      "Content-Type": "application/json",
+    }
+  }
+  const response = await fetch(`${basePath}/get-my-licenses`, params)
+  const data = await response.json()
+  return { response, data }
+}
