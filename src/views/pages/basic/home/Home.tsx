af815971407cb5fbb9606000601340247a693c8f
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2024-02-25 22:34:54 +0100

    change: add EMC logo

--- src/views/pages/basic/home/Home.tsx
+++ src/views/pages/basic/home/Home.tsx
@@ -3,12 +3,15 @@ import './Home.scss'
 import { memo } from 'react'
 import MyLicenses from 'views/components/basic/myAccount/myLicenses'
 import { useDauth } from 'dauth-context-react'
+import EMClogo from 'assets/img/emc-full-logo-360.png';
 
 function Home() {
   const { isAuthenticated } = useDauth()
   return (
     <div className='web-home'>
-      <h1>Easy Media Cloud</h1>
+      <div className='web-home__logo'>
+        <img src={EMClogo} alt="Easy Media Cloud" />
+      </div>
       <ApiBasic />
       {isAuthenticated && (
         <MyLicenses />
