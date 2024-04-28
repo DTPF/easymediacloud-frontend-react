627c71dd6c5dbf9acde19d5770cd1f57ddd1c401
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2024-04-28 13:51:08 +0200

    feat: create docs

--- src/context/licenses/LicensesProvider.tsx
+++ src/context/licenses/LicensesProvider.tsx
@@ -8,42 +8,75 @@ import { ILicenseState } from 'interfaces/license.interface'
 import { useDauth } from 'dauth-context-react'
 import { useTranslation } from 'react-i18next'
 
+/**
+ * Provides the context and actions related to licenses.
+ * @param props - The children props.
+ * @returns The LicensesProvider component.
+ */
 function LicensesProvider(props: ChildrenProps) {
 	const [lState, dispatch] = useReducer(licenseReducer, initialLicensesState)
 	const licensesState: ILicenseState = lState
 	const { getAccessToken } = useDauth()
 	const { t: translate } = useTranslation()
 
+	/**
+	 * Retrieves the licenses.
+	 */
 	const getLicenses = useCallback(async () => {
 		const token = await getAccessToken()
 		action.getLicensesAction(dispatch, token, translate)
 	}, [getAccessToken, translate])
 
+	/**
+	 * Creates a new license.
+	 * @param projectName - The name of the project.
+	 */
 	const postLicense = useCallback(async ({ projectName }: { projectName: string }) => {
 		const token = await getAccessToken()
 		action.postLicenseAction(dispatch, projectName, token, translate)
 	}, [getAccessToken, translate])
 
+	/**
+	 * Sets the online status of a license.
+	 * @param licenseId - The ID of the license.
+	 * @param online - The online status.
+	 */
 	const setLicenseOnline = useCallback(async ({ licenseId, online }: { licenseId: string, online: boolean }) => {
 		const token = await getAccessToken()
 		action.setLicenseOnlineAction(dispatch, licenseId, online, token, translate)
 	}, [getAccessToken, translate])
 
+	/**
+	 * Deletes a license.
+	 * @param licenseId - The ID of the license.
+	 */
 	const deleteLicense = useCallback(async ({ licenseId }: { licenseId: string }) => {
 		const token = await getAccessToken()
 		action.deleteLicenseAction(dispatch, licenseId, token, translate)
 	}, [getAccessToken, translate])
 
+	/**
+	 * Retrieves the access token for a license.
+	 * @param licenseId - The ID of the license.
+	 */
 	const getLicenseToken = useCallback(async ({ licenseId }: { licenseId: string }) => {
 		const token = await getAccessToken()
 		action.getLicenseTokenAction(licenseId, token, translate)
 	}, [getAccessToken, translate])
 
+	/**
+	 * Refreshes the access token for a license.
+	 * @param licenseId - The ID of the license.
+	 */
 	const refreshLicenseToken = useCallback(async ({ licenseId }: { licenseId: string }) => {
 		const token = await getAccessToken()
 		action.refreshLicenseTokenAction(licenseId, token, translate)
 	}, [getAccessToken, translate])
 
+	/**
+	 * Retrieves the media associated with a license.
+	 * @param licenseId - The ID of the license.
+	 */
 	const getLicenseMedia = useCallback(async ({ licenseId }: { licenseId: string }) => {
 		const token = await getAccessToken()
 		action.getLicenseMediaAction(dispatch, licenseId, token, translate)
